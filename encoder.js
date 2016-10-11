974a5fdfdc875a4013d138b41f4cc3a9be4df26d
Carlos Casas <albertocasas05@gmail.com>
2016-10-11 09:17:00 -0700

    added some

--- encoder.js
+++ encoder.js
@@ -1,3 +1,47 @@
+//my name
+$("#inputArea").keydown(function(e) {
   if(e.keyCode == 67) {
+    $("#textArea").append("c");
+  }
+});
+
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 65) {
+    $("#textArea").append("a");
+  }
+});
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 82) {
+    $("#textArea").append("r");
+  }
+});
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 76) {
+    $("#textArea").append("l");
+  }
+});
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 79) {
+    $("#textArea").append("o");
+  }
+});
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 83) {
+    $("#textArea").append("s" + " ");
+  }
+});
+
+//check/uncheck
+$("input[type='radio']").click(function(e) {
+  $("input:checked").prop('checked', false);
+  $(this).prop('checked', true);
+  if ($("input:checked").val("echo") == "echo") {
+    $("#textArea").append();
+  }
+  else if ($("input:checked").val("Caesar cipher") == "echo") {
+    $("#textArea").append();
+  }
+  else {
+    $("#textArea").append();
   }
 });
